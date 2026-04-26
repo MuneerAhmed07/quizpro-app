de9e413137df996ee5202d3f9edae6d8ad8dae7c
MuneerAhmed07 <muneerahmedk37@gmail.com>
2026-04-26 11:32:37 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Footer } from './components/Navigation';
+
 import Home from './pages/Home';
 import Subject from './pages/Subject';
 import Topic from './pages/Topic';
@@ -8,22 +9,38 @@ import About, { Contact, Privacy, Terms } from './pages/About';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/quizpro-app/">
       <div className="flex flex-col min-h-screen">
+
         <Navbar />
+
         <main className="flex-grow">
           <Routes>
+
+            {/* Home */}
+            <Route index element={<Home />} />
             <Route path="/" element={<Home />} />
+
+            {/* Subjects */}
             <Route path="/subject/:subjectId" element={<Subject />} />
+
+            {/* Topics */}
             <Route path="/subject/:subjectId/topic/:topicId" element={<Topic />} />
+
+            {/* Quiz */}
             <Route path="/quiz/:subjectId/:topicId/:quizId" element={<Quiz />} />
+
+            {/* Static Pages */}
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
+
           </Routes>
         </main>
+
         <Footer />
+
       </div>
     </Router>
   );
